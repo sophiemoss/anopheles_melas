fb68a3772b8da220406ff7abedcf398d096d271b
Sophie Moss <sophie.moss04@gmail.com>
2025-05-09 13:21:49 +0000

    run fastq2matrix edit

--- 1.runfastq2vcf_melas.sh
+++ 1.runfastq2vcf_melas.sh
@@ -13,3 +13,8 @@ cat fastq2vcfsamples.txt | parallel -j 30 \
 "/mnt/storage11/sophie/fastq2matrix/scripts/fastq2vcf.py all --read1 {}_1.fastq.gz --read2 {}_2.fastq.gz \
 --ref VectorBase-62_AmelasCM1001059_A_Genome.fasta \
 --prefix {}" > fastq2vcf_log.txt 2>&1
+
+cat fastq2vcfsamples.txt | parallel -j 30 \
+"/mnt/storage11/sophie/fastq2matrix/scripts/fastq2vcf.py all --read1 {}_1.fastq.gz --read2 {}_2.fastq.gz \
+--ref Pfalciparum.genome.fasta \
+--prefix {}" > fastq2vcf_log.txt 2>&1
